e154f78521c36f7125a452c2159fc84e4956a79c
weifangliu <wliu@macalester.edu>
2017-12-04 14:56:15 -0600

    Implement code from https://en.wikibooks.org/wiki/Algorithm_Implementation/Simulation/Monty_Hall_problem#Python

---
+++ Simulation1.py
@@ -0,0 +1,46 @@
+import sys,random
+
+rounds = 10000
+wins = 0
+
+random.seed()
+
+# The "-n" commandline option makes us run without ever switching.
+if len(sys.argv) > 1 and sys.argv[1] == "-n":
+    swap = False
+else:
+    swap = True
+
+for i in range(rounds) :
+
+    # Generate random door contents
+    doors = ["goat", "goat", "car"]
+    random.shuffle(doors)
+
+    # Pick a door
+    door_choice = random.randrange(3)
+    print("Selecting door", door_choice)
+
+    # Show a door with a goat
+    for j, contents in enumerate(doors):
+        if j != door_choice and contents == "goat":
+            goat_door = j
+            print("The host reveals a goat behind door", goat_door)
+            break
+
+    if swap:
+        # Swap to the other door
+        for j, contents in enumerate(doors):
+            if j != door_choice and j != goat_door:
+                swap_to = j
+                print("Swapping to door", swap_to)
+    else:
+        swap_to = door_choice
+
+    if doors[swap_to] == "car":
+        wins += 1
+        print ("You won the car!!")
+    else:
+        print ("Sorry, but you're stuck with a goat")
+
+print ("You played", rounds, "rounds, and won", wins, "of them!")
