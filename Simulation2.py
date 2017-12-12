7a25597a4e2ed8e3b8966e2fb25b52ecf22d5fb0
Yueyi Li <kateliyueyi2018@gmail.com>
2017-12-12 10:47:22 -0600

    Added num_doors

--- Simulation2.py
+++ Simulation2.py
@@ -4,7 +4,7 @@
 
 import argparse, random
 
-def simulate(num_doors, switch, verbose):
+def simulate(num_doors, switch):
     """(int, bool): bool
 
     Carry out the game for one contestant.  If 'switch' is True,
@@ -16,38 +16,25 @@ def simulate(num_doors, switch, verbose):
 
     # Randomly choose the door hiding the prize.
     winning_door = random.randint(0, num_doors-1)
-    if verbose:
-        print('Prize is behind door {}'.format(winning_door+1))
+
 
     # The contestant picks a random door, too.
     choice = random.randint(0, num_doors-1)
-    if verbose:
-        print('Contestant chooses door {}'.format(choice+1))
-
     # The host opens all but two doors.
     closed_doors = list(range(num_doors))
-    while len(closed_doors) > 2:
+
+    while True:
         # Randomly choose a door to open.
         door_to_remove = random.choice(closed_doors)
+        if door_to_remove !=winning_door and door_to_remove !=choice:
+            break
 
-        # The host will never open the winning door, or the door
-        # chosen by the contestant.
-        if door_to_remove == winning_door or door_to_remove == choice:
-            continue
 
-        # Remove the door from the list of closed doors.
-        closed_doors.remove(door_to_remove)
-        if verbose:
-            print('Host opens door {}'.format(door_to_remove+1))
-
-    # There are always two doors remaining.
-    assert len(closed_doors) == 2
+    # Remove the door from the list of closed doors.
+    closed_doors.remove(door_to_remove)
 
     # Does the contestant want to switch their choice?
     if switch:
-        if verbose:
-            print('Contestant switches from door {} '.format(choice+1), end='')
-
         # There are two closed doors left.  The contestant will never
         # choose the same door, so we'll remove that door as a choice.
         available_doors = list(closed_doors) # Make a copy of the list.
@@ -55,16 +42,11 @@ def simulate(num_doors, switch, verbose):
 
         # Change choice to the only door available.
         choice = available_doors.pop()
-        if verbose:
-            print('to {}'.format(choice+1))
+
 
     # Did the contestant win?
     won = (choice == winning_door)
-    if verbose:
-        if won:
-            print('Contestant WON', end='\n\n')
-        else:
-            print('Contestant LOST', end='\n\n')
+
     return won
 
 
@@ -72,12 +54,11 @@ def main():
     # Get command-line arguments
     parser = argparse.ArgumentParser(
         description='simulate the Monty Hall problem')
-    parser.add_argument('--doors', default=3, type=int, metavar='int',
+    parser.add_argument('--doors', default=10, type=int, metavar='int',
                         help='number of doors offered to the contestant')
     parser.add_argument('--trials', default=10000, type=int, metavar='int',
                         help='number of trials to perform')
-    parser.add_argument('--verbose', default=False, action='store_true',
-                        help='display the results of each trial')
+
     args = parser.parse_args()
 
     print('Simulating {} trials...'.format(args.trials))
@@ -87,12 +68,12 @@ def main():
     winning_switchers = 0
     for i in range(args.trials):
         # First, do a trial where the contestant never switches.
-        won = simulate(args.doors, switch=False, verbose=args.verbose)
+        won = simulate(args.doors, switch=False)
         if won:
             winning_non_switchers += 1
 
         # Next, try one where the contestant switches.
-        won = simulate(args.doors, switch=True, verbose=args.verbose)
+        won = simulate(args.doors, switch=True)
         if won:
             winning_switchers += 1
 
